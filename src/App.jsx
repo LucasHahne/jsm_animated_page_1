65b14ac0b2d4fe23800c80a5aa4d69eeac63b0eb
Lucas Hahne <lucas-hahne@web.de>
2025-02-11 18:44:13 +0100

    Final Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header></Header>
       <Hero></Hero>
+      <Features></Features>
+      <Pricing></Pricing>
+      <Faq></Faq>
     </main>
   );
 };
